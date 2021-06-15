2b53d775d4537c04d60e7b9c0e09989afd88caef
Jeffrey Lanters <jeffreylanters@me.com>
2021-06-15 15:44:39 +0200

    Added validate pull request actions

--- .actions/validate-pull-request.sh
+++ .actions/validate-pull-request.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 clear
+INVALID=false
 CHANGES=$(git diff | grep '^[+|-][^+|-]' -- README.md)
-VALIDATION=0
-HAS_ERRORS=false
 while IFS= read -r CHANGE; do
   if [[ $CHANGE =~ "- [http" ]]; then
     if [[ $CHANGE =~ "github.com/" ]]; then
       echo "OK"
     else
-      echo "ERROR: Added invalid URL in:\n\t$CHANGE"
-      HAS_ERRORS=true
+      echo "ERROR: Invalid URL in:\n\t$CHANGE"
+      INVALID=true
     fi
   else
     echo "ERROR: Invalid change in:\n\t$CHANGE"
-    HAS_ERRORS=true
+    INVALID=true
   fi
 done <<< "$CHANGES"
-if [[ $HAS_ERRORS == true ]]; then
+if [[ $INVALID == true ]]; then
   exit 1
 fi
